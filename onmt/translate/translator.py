165191bd4938c50c79c04677a43e2e82172245d5
chq <henry.q.cheng@gmail.com>
2020-05-11 15:47:01 +1000

    fix grad

--- onmt/translate/translator.py
+++ onmt/translate/translator.py
@@ -228,7 +228,7 @@ class Translator(object):
         # Help functions for working with beams and batches
         def var(a):
             # return torch.tensor(a, requires_grad=False)
-            return a.clone().detach().requires_grad_(True)
+            return a.clone().detach().requires_grad_(False)
 
         def rvar(a):
             return var(a.repeat(1, beam_size, 1))
